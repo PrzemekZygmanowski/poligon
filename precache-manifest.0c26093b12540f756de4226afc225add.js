bc74a717b177eef398a9db6573dca8f97608eda3
Zygmaister <Zygmaister@zygmaister.pl>
2020-11-06 18:08:50 +0100

    Updates

---
+++ precache-manifest.0c26093b12540f756de4226afc225add.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fac11f196d96805c271d633bbe5f793",
+    "url": "/nazwa/index.html"
+  },
+  {
+    "revision": "2098eba99398a7a4eb7c",
+    "url": "/nazwa/static/css/main.ffc26f15.chunk.css"
+  },
+  {
+    "revision": "a1330449f244418016ff",
+    "url": "/nazwa/static/js/2.a79fdeb6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/nazwa/static/js/2.a79fdeb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2098eba99398a7a4eb7c",
+    "url": "/nazwa/static/js/main.5f22dd27.chunk.js"
+  },
+  {
+    "revision": "0679a278c0eee65d2ca5",
+    "url": "/nazwa/static/js/runtime-main.32cdfe89.js"
+  },
+  {
+    "revision": "70b91d7a769def85e68ee30184eb4db5",
+    "url": "/nazwa/static/media/coffe.70b91d7a.jpg"
+  }
+]);
